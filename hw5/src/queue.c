49e503a15672451a935ec5206b4ab82ad1f5dce7
Feazan Yaseen <fyaseen@cs.stonybrook.edu>
2017-11-20 14:52:43 -0500

    Finished create_queue

--- hw5/src/queue.c
+++ hw5/src/queue.c
@@ -1,10 +1,23 @@
 #include "queue.h"
+#include <errno.h>
 
-queue_t *create_queue(void) {
-    return NULL;
+queue_t *create_queue(void)
+{
+    // TODO: How many should there be in the beginning? -- Assume 1
+    queue_t *my_queue = calloc(1, sizeof(queue_t));
+
+    if (my_queue == NULL
+        || pthread_mutex_init(&my_queue->lock, 0) != 0
+        || sem_init(&my_queue->items, 0, 0) != 0)
+    {
+        return NULL;
+    }
+
+    return my_queue;
 }
 
-bool invalidate_queue(queue_t *self, item_destructor_f destroy_function) {
+bool invalidate_queue(queue_t *self, item_destructor_f destroy_function)
+{
     return false;
 }
 
